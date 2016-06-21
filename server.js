0552ecfd2cb7e37e749c52c5764fe8ea5d4f215e
Yau Keng <ngyaukeng@gmail.com>
2016-06-22 02:19:13 +0800

    - [NEW] - first commit.

---
+++ server.js
@@ -0,0 +1,28 @@
+var deployd = require('deployd');
+
+var server = deployd({
+  port: process.env.PORT || 9090,
+  env: 'staging',
+  db: {
+    host: 'ds053894.mongolab.com',
+    port: 53894,
+    name: 'cincaidb',
+    credentials: {
+      username: 'hendry91',
+      password: 'hendry5203'
+    }
+  }
+});
+
+server.listen();
+
+server.on('listening', function() {
+  console.log("Server is listening");
+});
+
+server.on('error', function(err) {
+  console.error(err);
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
+  });
+});
